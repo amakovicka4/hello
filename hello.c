2b9044ec7df895b6c9c9063ea31fed354cd0edf8
Aiden Makovicka <amakovicka4@huskers.unl.edu>
2023-08-24 10:04:40 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ *
+ * Aiden Makovicka
+ * 2023-08-22
+ *
+ * A simple hello world program in c
+ *
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Aiden Makovicka");
+
+    return 0;
+}
